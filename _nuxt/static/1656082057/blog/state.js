abe1c64cca1d22dce38ad7fb854301fae552af9a
Loren Mucha <lorenmucha@gmail.com>
2022-06-24 16:48:05 +0200

    d50d296

---
+++ _nuxt/static/1656082057/blog/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmy_page\u002F_nuxt\u002Fstatic\u002F1656082057",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog",config:{_app:{basePath:"\u002Fmy_page\u002F",assetsPath:"\u002Fmy_page\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"546f6a60"}}}}(null));
---
+++ _nuxt/static/1656082057/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/", {data:[{_img:{"/my_page/_ipx/_/author.png":"\u002Fmy_page\u002F_nuxt\u002Fimage\u002Fa280d7.png"}}],fetch:{},mutations:void 0});
