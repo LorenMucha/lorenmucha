3094bb12ac7f4c3f520a207ac8c4013f00fffd54
lorenmucha <loren@vertical-life.info>
2023-06-29 17:29:07 +0200

    feature: Add Slider with text

--- src/pages/ClimbingDiary.vue
+++ src/pages/ClimbingDiary.vue
@@ -1,15 +1,23 @@
 <script lang="ts">
+import GooglePlayBtn from '../components/button/GooglePlayBtn.vue'
+
 definePageMeta({
   layout: false,
 })
 
 export default {
-  name: 'Climbing diary',
-
+  name: 'Climbing Diary',
   setup() {
     const slides = [
-      '/images/climbing_diary/barchart.png', '/images/climbing_diary/tabelle.png', '/images/climbing_diary/linechart.png',
-      '/images/climbing_diary/routen.png', '/images/climbing_diary/projects.png', '/images/climbing_diary/filter.png',
+      {
+        url: '/images/climbing_diary/barchart.png',
+        text: 'Statistische Auswertung deiner Erfolge',
+      },
+      { url: '/images/climbing_diary/tabelle.png' },
+      { url: '/images/climbing_diary/linechart.png' },
+      { url: '/images/climbing_diary/routen.png' },
+      { url: '/images/climbing_diary/projects.png' },
+      { url: '/images/climbing_diary/filter.png' },
     ]
     return { slides }
   },
@@ -18,17 +26,38 @@ export default {
 
 <template>
   <div class="page m-0 bg-slate-100">
-    <section class="container">
+    <section class="container text-center">
       <h1>Climbing-Diary</h1>
-      <div>Schöne & umfangreiche Tracking-App deiner Erfolge beim Klettern und Bouldern.</div>
+      <div>
+        Schöne & umfangreiche Tracking-App deiner Erfolge beim Klettern und
+        Bouldern.
+      </div>
+      <button class="btn" href="#" title="Google Play">
+        <img
+          class="h-24 w-48 object-cover"
+          src="~assets/images/google_play.png"
+        >
+      </button>
       <Swiper
         class="mt-10"
-        :pagination="true"
         :navigation="true"
-        :modules="[SwiperAutoplay, SwiperEffectCreative, SwiperScrollbar, SwiperNavigation, SwiperPagination]"
+        :modules="[
+          SwiperAutoplay,
+          SwiperEffectCreative,
+          SwiperScrollbar,
+          SwiperNavigation,
+        ]"
       >
-        <SwiperSlide v-for="image in slides" :key="image">
-          <img class="object-cover w-96 rounded-t-lg object-scale-down row-span-3" :src="image">
+        <SwiperSlide v-for="(image, idx) in slides" :key="idx">
+          <div class="center">
+            <img
+              class="mx-auto w-96 rounded-t-lg object-cover object-scale-down"
+              :src="image.url"
+            >
+            <p class="mt-5 text-base">
+              {{ image.text }}
+            </p>
+          </div>
         </SwiperSlide>
       </Swiper>
     </section>
@@ -43,7 +72,7 @@ export default {
   font-size: 18px;
   font-size: 4rem;
   font-weight: bold;
-  font-family: 'Roboto', sans-serif;
+  font-family: "Roboto", sans-serif;
 }
 .swiper-wrapper {
   min-width: 100vh;
