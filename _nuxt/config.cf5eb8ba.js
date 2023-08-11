5543652f85d2530e5042e0929d06506831807da7
LorenMucha <lorenmucha@gmail.com>
2023-08-11 14:57:21 +0200

    Updates

---
+++ _nuxt/config.cf5eb8ba.js
@@ -0,0 +1,1 @@
+import{o as n,d as o}from"./entry.3cf701ce.js";import{r as i}from"./swiper-vue.c7fbfdd7.js";const f={},t=n(f);function s(){const p=o();return p._appConfig||(p._appConfig=i(t)),p._appConfig}export{s as u};
