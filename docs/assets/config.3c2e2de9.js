f31e60012279b95920be6171523b11c2e569abf7
LorenMucha <lorenmucha@gmail.com>
2023-08-13 17:55:27 +0200

    publish page

---
+++ docs/assets/config.3c2e2de9.js
@@ -0,0 +1,1 @@
+import{o as n,d as o}from"./entry.9c59b8c5.js";import{r as i}from"./swiper-vue.3715e06f.js";const f={},t=n(f);function s(){const p=o();return p._appConfig||(p._appConfig=i(t)),p._appConfig}export{s as u};
