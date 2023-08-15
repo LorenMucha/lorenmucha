486192fc713230ce9fc180def6d3c802cbf18770
LorenMucha <lorenmucha@gmail.com>
2023-08-15 13:40:03 +0200

    Updates

--- .nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, Ref } from 'vue'
-export type LayoutKey = "default"
-declare module "/home/loren/Entwicklung/Web/lorenmucha/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
-  }
-}
